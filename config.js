b4d256ae3f6a9532a26c9abdb089c6c095a0226b
Sparshshukla19 <152921093+Sparshshukla19@users.noreply.github.com>
2025-07-19 13:05:15 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+export const apiKey = "405f3d53a1f0c8cceafa45902064a7a6";
+export const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=matric&q=`;
---
+++ script.js
@@ -0,0 +1,57 @@
+import { apiKey, apiUrl} from "./config.js";
+
+const searchInput = document.querySelector(".search-input");
+const searchIcon = document.querySelector(".search-icon");
+const weatherIconImg = document.querySelector(".weather-icon-img");
+const temperatureEl = document.querySelector(".temperature");
+const cityNameEl = document.querySelector(".city-name");
+const humidityEl = document.querySelector(".humidity-details span");
+const windspeedEl = document.querySelector(".wind-speed-details span");
+
+function getWeatherImg(cloudiness){
+    if(cloudiness > 80){
+        return "./assets/rain.webp";
+        
+    }
+    else if(cloudiness > 30 ){
+       return "./assets/clouds.png";
+    }
+    else {
+        return "./assets/sunny.webp"
+    }
+}
+
+
+
+async function getWeather(city){
+    try{
+        const response = await fetch(`${apiUrl}${city}&appid=${apiKey}`);
+        if(!response.ok) throw new Error("city not found")
+         
+        const data = await response.json();
+        cityNameEl.textContent = data.name;;
+        temperatureEl.textContent = `${Math.round(data.main.temp - 273)}°C`;
+        windspeedEl.textContent = `${data.wind.speed} km/h`;
+        humidityEl.textContent = `${data.main.humidity}%`;
+        weatherIconImg.src = getWeatherImg(data.clouds.all);
+
+        // console.log(data);
+    }
+    catch(e){
+
+    }
+}
+
+searchIcon.addEventListener("click",()=>{
+    const city = searchInput.value.trim();
+    if(city){
+        getWeather(city);
+    }
+    else{
+        alert("Enter valid city");
+    }
+});
+
+
+
+getWeather("delhi");
